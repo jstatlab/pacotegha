2224b60a00a844339af5e6a92739bb59efedbcd4
jstat-lab <jsjale1983@gmail.com>
2023-04-17 18:41:40 -0300

    Initial commit

---
+++ R/somar_dois_numeros.R
@@ -0,0 +1,12 @@
+#' Somar dois numeros
+#'
+#' @param x Um numero
+#' @param y Outro numero
+#'
+#' @return Um numero
+#' @export
+#'
+#' @examples somar_dois_numeros(1, 1)
+somar_dois_numeros <- function(x, y) {
+  x + y
+}
